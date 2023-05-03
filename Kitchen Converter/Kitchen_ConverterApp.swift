996300bc0911c6af4cf01140e7df265a38efc042
Benjamin Lellouch <blellouch@gmail.com>
2023-05-03 14:27:37 +0200

    Initial Commit

---
+++ Kitchen Converter/Kitchen_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitchen_ConverterApp.swift
+//  Kitchen Converter
+//
+//  Created by Benjamin Lellouch on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
