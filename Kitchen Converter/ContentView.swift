c32b8d37ae3a17a13224223427f5be0368a08973
Benjamin Lellouch <blellouch@gmail.com>
2023-05-05 01:25:45 +0200

    Added more conversions and some ui changes

--- Kitchen Converter/ContentView.swift
+++ Kitchen Converter/ContentView.swift
@@ -15,25 +15,37 @@ struct Unit: Hashable, Identifiable, Equatable {
     var id: Self {self}
     let name: String
     let symbol: String
-    let type: UnitType
+    let type: [UnitType]
 }
 
 struct ContentView: View {
     
     static let units = [
-        Unit(name:"Grams", symbol: "g", type:UnitType.mass),
-        Unit(name:"Ounces", symbol: "oz", type:UnitType.mass),
-        Unit(name: "Fluid Ounces", symbol: "fl oz", type: UnitType.vol),
-        Unit(name: "Milliliters", symbol: "ml", type: UnitType.vol),
-        Unit(name: "Celsius", symbol: "°C", type: UnitType.temp),
-        Unit(name: "Farenheit", symbol: "°F", type: UnitType.temp)
+        Unit(name:"Grams", symbol: "g", type:[UnitType.mass]),
+        Unit(name:"Ounces", symbol: "oz", type:[UnitType.mass]),
+        Unit(name: "Fluid Ounces", symbol: "fl oz", type: [UnitType.vol]),
+        Unit(name: "Milliliters", symbol: "ml", type: [UnitType.vol]),
+        Unit(name: "Celsius", symbol: "°C", type: [UnitType.temp]),
+        Unit(name: "Farenheit", symbol: "°F", type: [UnitType.temp]),
+        Unit(name: "Butter Sticks", symbol: "stick", type: [UnitType.vol, UnitType.mass]),
+        Unit(name: "Tablespoon", symbol: "tbsp", type: [UnitType.vol]),
+        Unit(name: "Cup", symbol: "cup", type: [UnitType.vol]),
+        Unit(name: "Teaspoon", symbol: "tsp", type: [UnitType.vol])
+        
         
     ]
     
-    static let conversions = [
-        "g": ["oz":{x in x * 0.035274}],
-        "ml": ["fl oz":{x in x * 0.033814}],
-        "°C": ["°F":{x in (x * 9/5) + 32.0}]
+    static let conversions: [String:[String:(Double) -> Double]] = [
+        "g": ["oz":{x in x * 0.035274}, "stick":{x in x * (1/113)}],
+        "stick": ["g": {x in x * 113}, "oz": {x in x * 113 * 0.035274}, "cup": {x in x/2}, "tbsp": {x in x * 8}, "fl oz": {x in x * 4}, "ml":{x in x * 111.294}],
+        "oz": ["g":{x in x * (1/0.035274)}, "stick":{x in x * (1/0.035274) * (1/113)}],
+        "ml": ["fl oz":{x in x * 0.033814}, "cup":{x in x * 0.00422675}, "tbsp": {x in x * 0.067628}],
+        "fl oz": ["ml":{x in x * (1/0.033814)}, "cup": {x in x * 1/8}, "tbps": {x in x * 2}, "stick":{x in x/4}],
+        "°C": ["°F":{x in (x * 9/5) + 32.0}],
+        "°F": ["°C":{x in (x - 32) * 5/9}],
+        "tbsp": ["ml": {x in x * (1/0.067628)} ,"cup": {x in x * 1/16}, "fl oz": {x in x * 0.5}, "stick" : {x in x/8} ],
+        "tsp": ["tbsp": {x in x/3}],
+        "cup": ["ml": {x in x * (1/0.00422675)}, "tbsp": {x in x * 16}, "fl oz": {x in x * 8}, "stick" : {x in x*2}]
         
     ]
     @State private var from: Unit = units[0]
@@ -43,46 +55,70 @@ struct ContentView: View {
         return ((ContentView.conversions[from.symbol]?[to.symbol] ?? {x in 0})(Double(input) ?? 0))
     }
     private var filteredUnits: [Unit] {
-        switch from.type {
-        case UnitType.mass:
-            let fs = ContentView.units.filter{$0.type == UnitType.mass && $0.name != from.name}
-            return fs
-        case UnitType.vol:
-            let fs = ContentView.units.filter{$0.type == UnitType.vol && $0.name != from.name}
-            return fs
-        case UnitType.temp:
-            let fs = ContentView.units.filter{$0.type == UnitType.temp && $0.name != from.name}
-            return fs
+        var fs : [Unit] = []
+        for type in from.type{
+            switch type {
+            case UnitType.mass:
+                fs.append(contentsOf: ContentView.units.filter{$0.type.contains(UnitType.mass)  && $0.name != from.name})
+                break
+            case UnitType.vol:
+                fs.append(contentsOf:  ContentView.units.filter{$0.type.contains(UnitType.vol) && $0.name != from.name})
+                break
+            case UnitType.temp:
+                fs.append(contentsOf:  ContentView.units.filter{$0.type.contains(UnitType.temp) && $0.name != from.name})
+                break
+            }
         }
+        return fs
     }
         
     var body: some View {
         VStack {
+            Text("Unit Converter")
+                .font(.title)
+            Spacer()
             VStack{
-                Text("Converter")
+                
                 HStack{
                     Text("From")
+                        .font(.headline)
+
                     
                     Picker("From",selection: $from) {
                         ForEach(ContentView.units){ unit in
                             Text(unit.symbol).tag(unit)
                         }
                         
-                    }.onChange(of: from, perform: {value in if (from.type != to.type) {to = filteredUnits[0] }})
+                    }.onChange(of: from, perform: {value in if (from.type != to.type || from.name == to.name ) {to = filteredUnits[0] }}).frame(width: 80)
                     
                     Text("➡️")
                     Text("To")
+                        .font(.headline)
                     Picker("To",selection: $to) {
                         ForEach(filteredUnits){ unit in
                             Text(unit.symbol).tag(unit)
                         }
-                    }
+                    }.frame(width: 80)
+                }.padding()
+                HStack {
+                    TextField("", text: $input).numbersOnly($input, includeDecimal: true).textFieldStyle(.roundedBorder).frame(minWidth: 50,idealWidth: 50,maxWidth:70)
+                    Text("\(from.symbol)")
+                        .font(.headline)
+
+                    Text("➡️")
+
+                    Text("\( String(format: "%.2f", output))").padding(6)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                    Text("\(to.symbol)")
+                        .font(.headline)
+
                 }
-                Text("You are converting \(from.name) to \(to.name)")
-                TextField("Input unit", text: $input).numbersOnly($input, includeDecimal: true)
-                Text("Converted unit: \(output)")
             }.padding()
-            VStack{Text("For Mamou ♥️ by Ben")}.frame(alignment: .bottom)
+            Spacer()
+            VStack{Text("Made with ♥️ for Mamou")}.frame(alignment: .bottom)
             
         }
         .padding()
