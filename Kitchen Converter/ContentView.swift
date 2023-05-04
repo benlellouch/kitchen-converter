10169fd5ea095b5056d4e334b7f3897a409e3a00
Benjamin Lellouch <blellouch@gmail.com>
2023-05-04 15:04:07 +0200

    Added conversion between units

--- Kitchen Converter/ContentView.swift
+++ Kitchen Converter/ContentView.swift
@@ -11,10 +11,8 @@ enum UnitType {
     case mass,vol,temp
 }
 
-struct Unit: Hashable, Identifiable {
+struct Unit: Hashable, Identifiable, Equatable {
     var id: Self {self}
-    
-    
     let name: String
     let symbol: String
     let type: UnitType
@@ -26,52 +24,70 @@ struct ContentView: View {
         Unit(name:"Grams", symbol: "g", type:UnitType.mass),
         Unit(name:"Ounces", symbol: "oz", type:UnitType.mass),
         Unit(name: "Fluid Ounces", symbol: "fl oz", type: UnitType.vol),
-        Unit(name: "Milliliters", symbol: "ml", type: UnitType.vol)
+        Unit(name: "Milliliters", symbol: "ml", type: UnitType.vol),
+        Unit(name: "Celsius", symbol: "°C", type: UnitType.temp),
+        Unit(name: "Farenheit", symbol: "°F", type: UnitType.temp)
+        
+    ]
+    
+    static let conversions = [
+        "g": ["oz":{x in x * 0.035274}],
+        "ml": ["fl oz":{x in x * 0.033814}],
+        "°C": ["°F":{x in (x * 9/5) + 32.0}]
+        
     ]
     @State private var from: Unit = units[0]
     @State private var to: Unit = units[1]
     @State private var input: String = "0"
+    private var output: Double {
+        return ((ContentView.conversions[from.symbol]?[to.symbol] ?? {x in 0})(Double(input) ?? 0))
+    }
+    private var filteredUnits: [Unit] {
+        switch from.type {
+        case UnitType.mass:
+            let fs = ContentView.units.filter{$0.type == UnitType.mass && $0.name != from.name}
+            return fs
+        case UnitType.vol:
+            let fs = ContentView.units.filter{$0.type == UnitType.vol && $0.name != from.name}
+            return fs
+        case UnitType.temp:
+            let fs = ContentView.units.filter{$0.type == UnitType.temp && $0.name != from.name}
+            return fs
+        }
+    }
         
     var body: some View {
         VStack {
-            Text("Converter")
-            HStack{
-                Text("From")
-                Picker("From",selection: $from) {
-                    ForEach(ContentView.units){ unit in
-                        Text(unit.symbol)
+            VStack{
+                Text("Converter")
+                HStack{
+                    Text("From")
+                    
+                    Picker("From",selection: $from) {
+                        ForEach(ContentView.units){ unit in
+                            Text(unit.symbol).tag(unit)
+                        }
+                        
+                    }.onChange(of: from, perform: {value in if (from.type != to.type) {to = filteredUnits[0] }})
+                    
+                    Text("➡️")
+                    Text("To")
+                    Picker("To",selection: $to) {
+                        ForEach(filteredUnits){ unit in
+                            Text(unit.symbol).tag(unit)
+                        }
                     }
-
                 }
-                Text("➡️")
-                Text("To")
-                Picker("To",selection: $to) {
-                    let filteredUnits = filterUnits(unit: from, list: ContentView.units)
-                    ForEach(filteredUnits){ unit in
-                        Text(unit.symbol)
-                    }
-                }
-            }
-            Text("You are converting \(from.name) to \(to.name)")
-            TextField("Input unit", text: $input).numbersOnly($input, includeDecimal: true)
+                Text("You are converting \(from.name) to \(to.name)")
+                TextField("Input unit", text: $input).numbersOnly($input, includeDecimal: true)
+                Text("Converted unit: \(output)")
+            }.padding()
+            VStack{Text("For Mamou ♥️ by Ben")}.frame(alignment: .bottom)
+            
         }
         .padding()
     }
     
-    func filterUnits(unit: Unit, list: [Unit]) -> [Unit]{
-        switch unit.type {
-        case UnitType.mass:
-            let fs = list.filter{$0.type == UnitType.mass}
-            return fs
-        case UnitType.vol:
-            let fs = list.filter{$0.type == UnitType.vol}
-            return fs
-        case UnitType.temp:
-            let fs = list.filter{$0.type == UnitType.temp}
-            return fs
-        }
-        
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
